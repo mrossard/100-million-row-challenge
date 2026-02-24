7309f30a74064c4f626ae5bef2ec8263ec753919
Manuel Rossard <manuel.rossard@u-bordeaux.fr>
2026-02-24 15:55:45 +0100

    base version

--- app/Parser.php
+++ app/Parser.php
@@ -2,12 +2,37 @@
 
 namespace App;
 
-use Exception;
+use function file_put_contents;
+use function json_encode;
+use function ksort;
+use function stream_get_line;
+use function substr;
+use function strlen;
+use function strpos;
 
 final class Parser
 {
     public function parse(string $inputPath, string $outputPath): void
     {
-        throw new Exception('TODO');
+        $handle = fopen($inputPath, 'r');
+
+        $baseUrl = 'https://stitcher.io';
+        $baseUrlLength = strlen($baseUrl);
+
+        $data = [];
+        while($line = stream_get_line($handle, 4096, \PHP_EOL)) {
+            $comma = strpos($line, ',');
+            $path = substr($line, $baseUrlLength, $comma - $baseUrlLength);
+            $year = substr($line, -25, 10);
+            $data[$path][$year] = ($data[$path][$year] ?? 0) + 1;
+        }
+
+        foreach($data as &$paths) {
+            ksort($paths);
+        }
+
+        file_put_contents($outputPath, json_encode($data, \JSON_PRETTY_PRINT));
     }
+
+
 }
